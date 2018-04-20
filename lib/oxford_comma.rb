65f6a9a392b3338ea0c375b01dfa19f2bf0e2750
Caio Sanches <ccampato@gmail.com>
2018-04-20 06:28:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   puts last_word = array.pop
   puts string_conversion = array.join(", ")
   puts string_conversion << " and "
+  puts string_conversion << last_word
 end
 
 fruits = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
