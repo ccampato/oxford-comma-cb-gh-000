2a1bcac21d8d1cfb596cce43b1da93c06cd399e3
Caio Sanches <ccampato@gmail.com>
2018-04-20 06:26:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   puts last_word = array.pop
-  puts last_word << "and "
+  puts last_word.unshift("and ")
   string_conversion = array.join(", ")
   string_conversion << last_word
 end
