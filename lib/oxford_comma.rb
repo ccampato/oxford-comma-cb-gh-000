d3221281aa8de3162f1dcba5b1f1ac89e0f0186d
Caio Sanches <ccampato@gmail.com>
2018-04-20 06:18:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   last_word = array.pop
-  last_word.push(" and ")
+  last_word.unshift(" and ")
+  last_word.join
   string_conversion = array.join(", ")
   string_conversion << last_word
 end
